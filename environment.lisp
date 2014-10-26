3e6089dadfe8fa5d8322fd097ba18c7d82f72a96
Shinmera <shinmera@tymoon.eu>
2014-10-26 13:51:43 +0100

    Environment macro stuff in own file, changed the system to be more flexible.

---
+++ environment.lisp
@@ -0,0 +1,50 @@
+#|
+ This file is a part of Qtools
+ (c) 2014 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.qtools)
+
+(defvar *environment-forms* (make-hash-table :test 'equal))
+
+(defun environment-form (name)
+  (or (gethash (string name) *environment-forms*)
+      #'identity))
+
+(defun (setf environment-form) (function name)
+  (setf (gethash (string name) *environment-forms*)
+        function))
+
+(defmacro define-environment-form (name structure &body body)
+  (let ((form (gensym "FORM")))
+    `(setf (environment-form ',name)
+           #'(lambda (,form)
+               (destructuring-bind ,structure ,form
+                 ,@body)))))
+
+(defmacro define-environment-form-class-option (name option)
+  (assert (keywordp option) () "Option must be a keyword!")
+  `(define-environment-form ,name (&rest body)
+     (values
+      NIL
+      (list (list ,option body)))))
+
+(define-environment-form-class-option :signal)
+(define-environment-form-class-option :slot)
+(define-environment-form-class-option :overrides)
+(define-environment-form-class-option :widget)
+(define-environment-form-class-option :layout)
+
+(defmacro with-qt-environment (&body forms)
+  (loop for (function . body) in forms
+        for (forms options) = (multiple-value-list (environment-form function))
+        collect forms into all-forms
+        append options into all-options
+        finally (return
+                  (let ((classdef (find 'define-qt-widget all-forms :key #'car)))
+                    (when (and all-options (not classdef))
+                      (warn "Class body forms found but no class definition!"))
+                    `(progn
+                       ,@(when classdef (list (append classdef all-options)))
+                       ,@(remove 'define-qt-widget all-forms :key #'car))))))
