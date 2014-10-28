3e59bbbb152ea6d8888628ffb52de422c44b7624
Shinmera <shinmera@tymoon.eu>
2014-10-28 15:30:35 +0100

    Changed THIS to WIDGET binding, added docstrings.

--- widget-options.lisp
+++ widget-options.lisp
@@ -12,6 +12,18 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
 (defvar *layout-init-priority* 30)
 
 (define-widget-class-option :defsignals (class name args &rest decls)
+  "Allows a more handy definition of Qt signals. 
+See the CommonQt :signals class option.
+
+Expected is a list of the following structure:
+
+BODY        ::= NAME (ARG*) DECLARATION*
+ARG         ::= QT-TYPE | (QT-TYPE*)
+DECLARATION ::= (declare (method [NAME]))
+
+A METHOD declaration has the effect of generating
+signalling methods for the signal. See
+DEFIEN-SIGNAL-METHOD."
   (destructuring-bind (declarations methods) (split decls '(method) :key #'caadr :test #'string=)
     (declare (ignore declarations))
     (when methods
@@ -23,6 +35,26 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
   `(:signals ,(mapcar #'list (enumerate-method-descriptors (to-method-name name) args))))
 
 (define-widget-class-option :defslots (class &rest body)
+  "Allows a more handy definition of Qt slots.
+See the CommonQt :slots class option.
+
+Expected is a list of the following structure:
+
+BODY        ::= NAME (ARG*) [[DOCSTRING | DECLARATION*]] FORM*
+ARG         ::= SYMBOL | (SYMBOL QT-TYPE*)
+DECLARATION ::= CL-DECLARATION
+                | (declare (connected OBJECT FUNCTION))
+                | (declare (method [NAME])
+
+A CONNECTED declaration has the effect of automatically
+executing a CONNECT! statement on instantiation.
+
+A METHOD declaration has the effect of generating corresponding
+methods instead of lambda-bodies for the slot function.
+
+The body of the function is automatically wrapped in
+WITH-SLOTS-BOUND, making all direct class-slots bound to symbols
+of the slot-name."
   ;; Split apart our lambda
   (form-fiddle:with-destructured-lambda-form (:name name :lambda-list args :docstring doc :declarations decls :forms forms) (cons :slot body)
     ;; Filter declarations
@@ -65,6 +97,15 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
           `(:defslots ,(mapcar #'build-slot (enumerate-method-descriptors cpp-name cpp-args))))))))
 
 (define-widget-class-option :defoverrides (class name args &rest body)
+  "Allows a more handy definition of override methods.
+See the CommonQt :overrides class slot option.
+
+Expected is a list of the following structure:
+BODY ::= NAME ARGS FORM*
+
+The body of the function is automatically wrapped in
+WITH-SLOTS-BOUND, making all direct class-slots bound to symbols
+of the slot-name."
   `(:override
     ((,(to-method-name name)
       (lambda ,args
@@ -72,29 +113,72 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
           ,@body))))))
 
 (define-widget-slot-option :subwidget (class name constructor &rest body)
+  "Defines a slot on the class to store the subwidget. The slot has no readers or writers and is FINALIZED."
+  (declare (ignore class))
   `(:direct-slots ((:name ,name :readers () :writers () :initargs () :finalized T))
     :subwidget ((,name ,constructor ,@body))))
 
 (define-widget-class-option :subwidget (class name constructor &rest body)
+  "Defines a sub-widget on the widget.
+
+Expected is a list of the following structure:
+BODY ::= NAME CONSTRUCTOR-FORM FORM*
+
+When the class is instantiated, this form is evaluated in
+the following manner: All class-slots are bound as per 
+WITH-SLOTS-BOUND and the corresponding sub-widget
+slot is set to the resulting value of CONSTRUCTOR-FORM.
+Then the remaining body-forms are evaluated. These should
+mainly be used to set all the initial properties of the widget,
+to set up signal connections, etc.
+
+The main widget is bound to the symbol QTOOLS:WIDGET."
   (add-initializer
    class *widget-init-priority*
-   `(lambda (this)
-      (with-slots-bound (this ,class)
+   `(lambda (widget)
+      (with-slots-bound (widget ,class)
         (setf ,name ,constructor)
         ,@body)))
   NIL)
 
 (define-widget-class-option :layout (class name constructor &rest body)
+  "Shorthand macro to define the widget's layout.
+
+Expected is a list of the following structure:
+BODY ::= NAME CONSTRUCTOR-FORM FORM*
+
+When the class is instantiated and all sub-widgets have been
+initialised, this form is evaluated in the following manner:
+The slots are bound as per WITH-SLOTS-BOUND, the NAME symbol
+is bound to the value of the CONSTRUCTOR-FORM. The FORMs are
+evaluated and then #_setLayout is executed using NAME.
+
+The main widget is bound to the symbol QTOOLS:WIDGET"
   (add-initializer
    class *layout-init-priority*
-   `(lambda (this)
-      (with-slots-bound (this ,class)
+   `(lambda (widget)
+      (with-slots-bound (widget ,class)
         (let ((,name ,constructor))
           ,@body
-          (#_setLayout this ,name)))))
+          (#_setLayout widget ,name)))))
   NIL)
 
 (define-widget-class-option :initializer (class widget priority &rest body)
+  "Defines a function to run as an initializer.
+This is useful in order to interject things to be evaluated
+before or between the initialization steps of a widget class.
+
+Expected is a list of the following structure:
+BODY ::= WIDGET PRIORITY FORM*
+
+The main widget is bound to the WIDGET symbol and all its slots
+are bound as per WITH-SLOTS-BOUND. The initializer is evaluated
+in sequence according to its PRIORITY. The higher the value, the
+later in the process it is executed.
+
+See *WIDGET-INIT-PRIORITY*
+See *SLOT-INIT-PRIORITY*
+See *LAYOUT-INIT-PRIORITY*"
   (add-initializer
    class priority
    `(lambda (,widget)
