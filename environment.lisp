fca8393ec4f0ed1bc4ee753a14c4206d984846b2
Shinmera <shinmera@tymoon.eu>
2014-10-28 11:37:51 +0100

    Minor change to ENVIRONMENT-FORM, added LIST-ENVIRONMENT-FORMS and DESCRIBE-ENVIRONMENT-FORM.

--- environment.lisp
+++ environment.lisp
@@ -10,9 +10,8 @@
   "Table mapping form names to evaluator functions.")
 
 (defun environment-form (name)
-  "Returns a function to process the form of NAME with."
-  (or (gethash (string name) *environment-forms*)
-      #'identity))
+  "Returns a function to process the form of NAME with, if any.."
+  (gethash (string name) *environment-forms*))
 
 (defun (setf environment-form) (function name)
   "Sets a new evaluator function for the given form NAME."
@@ -21,9 +20,11 @@
 
 (defmacro define-environment-form (name structure &body body)
   "Define processing for an environment form of NAME with a body of STRUCTURE."
-  (let ((form (gensym "FORM")))
+  (let ((form (gensym "FORM"))
+        (doc (form-fiddle:lambda-docstring `(() ,@body))))
     `(setf (environment-form ',name)
            #'(lambda (,form)
+               ,@(when doc (list doc))
                (destructuring-bind ,structure (cdr ,form)
                  ,@body)))))
 
@@ -31,9 +32,20 @@
   "Shorthand macro to translate forms of NAME to widget class OPTION."
   (assert (keywordp option) () "Option must be a keyword!")
   `(define-environment-form ,name (&rest body)
+     ,(format NIL "Translates to the class option ~s" option)
      (values
       NIL
       (list (list ,option (list body))))))
+
+(defun list-environment-forms ()
+  "Lists all specially handled forms in a widget-environment."
+  (loop for key being the hash-keys of *environment-forms*
+        collect key))
+
+(defun describe-environment-form (form)
+  (let ((func (environment-form form)))
+    (format T "~a: ~:[No special handling, will evaluate to the same.~;~:[No documentation available.~;~:*~a~]~]"
+            form func (when func (documentation func T)))))
 
 (define-environment-form-class-option define-signal :defsignals)
 (define-environment-form-class-option define-slot :defslots)
@@ -47,7 +59,8 @@
 The main purpose of this macro is to avoid having to press all of your information into
 the class definition form."
   (loop for form in forms
-        for (forms options) = (multiple-value-list (funcall (environment-form (car form)) form))
+        for (forms options) = (multiple-value-list (funcall (or (environment-form (car form))
+                                                                #'identity) form))
         when forms
         collect forms into all-forms
         append options into all-options
