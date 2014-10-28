878a28138a242e0b145c43a5161122626ca56db0
Shinmera <shinmera@tymoon.eu>
2014-10-28 13:10:42 +0100

    Documentation

--- toolkit.lisp
+++ toolkit.lisp
@@ -11,10 +11,12 @@
 ;; Qt Related Utils
 
 (defun qobject-alive-p (object)
+  "Returns T if the object is not null and not deleted."
   (not (or (null-qobject-p object)
            (qobject-deleted object))))
 
 (defun maybe-delete-qobject (object)
+  "Deletes the object if possible."
   (if (typep object 'abstract-qobject)
       (when (qobject-alive-p object)
         #+:verbose (v:trace :qtools "Deleting QObject: ~a" object)
@@ -22,6 +24,8 @@
       #+:verbose (v:trace :qtools "Deleting QObject: WARN Tried to delete non-qobject ~a" object)))
 
 (defgeneric copy-qobject-using-class (qclass instance)
+  (:documentation "Creates a copy of the given instance by using methods
+appropriate for the given qclass.")
   #+:verbose
   (:method :before (qclass instance)
     (v:trace :qtools "Copying QObject: ~a" instance))
@@ -33,18 +37,31 @@
     (#_new QColor instance)))
 
 (defgeneric copy-qobject (instance)
-  (:documentation "Generates a copy of the")
+  (:documentation "Generates a copy of the qobject.
+Uses COPY-QOBJECT-USING-CLASS and determines the class by QT::QOBJECT-CLASS.")
   (:method (instance)
     (copy-qobject-using-class
      (qt::qobject-class instance) instance)))
 
 (defmacro qtenumcase (keyform &body forms)
+  "Just like CASE, but for Qt enums using QT:ENUM=."
   (let ((key (gensym "KEY")))
     `(let ((,key ,keyform))
-       (cond ,@(loop for form in forms
-                     collect `((qt:enum= ,key ,(car form)) ,@(cdr form)))))))
+       (cond ,@(loop for (comp . form) in forms
+                     collect (if (or (eql comp T)
+                                     (eql comp 'otherwise))
+                                 `(T ,@form)
+                                 `((qt:enum= ,key ,comp) ,@form)))))))
 
 (defun enumerate-method-descriptors (name args)
+  "Returns a list of all possible method descriptors with NAME and ARGS.
+Args may be either a list of direct types to use or a list of alternative types.
+In the case of lists, the argument alternatives are taken in parallel.
+
+Examples: 
+ (.. foo '(a b)) => (\"foo(a,b)\")
+ (.. foo '((a b))) => (\"foo(a)\" \"foo(b)\")
+ (.. foo '((a b) (0 1))) => (\"foo(a,0)\" \"foo(b,1)\")"
   (flet ((make-map (args)
            (format NIL "~a(~{~(~a~)~^, ~})" name args)))
     (cond
@@ -58,12 +75,17 @@
 ;; General utils
 
 (defun ensure-class (thing)
+  "Ensures to return a CLASS.
+SYMBOL -> FIND-CLASS
+CLASS  -> IDENTITY
+STANDARD-OBJECT -> CLASS-OF"
   (etypecase thing
     (symbol (find-class thing))
     (class thing)
     (standard-object (class-of thing))))
 
 (defmacro with-slots-bound ((instance class) &body body)
+  "Turns into a WITH-SLOTS with all direct-slots of CLASS."
   (let ((slots (loop for slot in (c2mop:class-direct-slots
                                   (ensure-class class))
                      for name = (c2mop:slot-definition-name slot)
@@ -93,18 +115,26 @@
           collect (cons key val))))
 
 (defun split (list items &key (key #'identity) (test #'eql))
+  "Segregates items in LIST into separate lists if they mach an item in ITEMS.
+The first item in the returned list is the list of unmatched items.
+
+Example:
+ (split '((0 a) (0 b) (1 a) (1 b) (2 c)) '(0 2) :key #'car)
+ => '(((1 a) (1 b)) ((0 a) (0 b)) ((2 c))) "
   (loop with table = ()
         for item in list
         do (push item (getf table (find (funcall key item) items :test test)))
-        finally (return (cons (getf table NIL)
+        finally (return (cons (nreverse (getf table NIL))
                               (loop for item in items
-                                    collect (getf table item))))))
+                                    collect (nreverse (getf table item)))))))
 
 (defmacro with-compile-and-run (&body body)
+  "Compiles BODY in a lambda and funcalls it."
   `(funcall
     (compile NIL `(lambda () ,,@body))))
 
 (defun maybe-unwrap-quote (thing)
+  "If it is a quote form, unwraps the contents. Otherwise returns it directly."
   (if (and (listp thing)
            (eql 'quote (first thing)))
       (second thing)
