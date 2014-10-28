7c213b31bb20e535659dbda3c62dcae620752436
Shinmera <shinmera@tymoon.eu>
2014-10-28 10:27:20 +0100

    Rewrote SLOT option to support method declarations

--- widget-options.lisp
+++ widget-options.lisp
@@ -15,30 +15,47 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
   `(:signals ,(mapcar #'list (enumerate-method-descriptors (to-method-name name) args))))
 
 (define-widget-class-option :slot (class &rest body)
-  (form-fiddle:with-destructured-lambda-form
-      (:name name :lambda-list args :docstring doc :declarations decls :forms forms) (cons :slot body)
-    (let* ((clean-decls (remove 'connected decls :key #'caadr :test #'eql))
-           (connections (remove 'connected decls :key #'caadr :test-not #'eql))
-           (clean-args (mapcar #'(lambda (a) (if (listp a) (car a) a)) args))
-           (this (first clean-args)))
-      (setf name (to-method-name name))
-
-      (when connections
-        (add-initializer
-         class *slot-init-priority*
-         `(lambda (,this)
-            (with-slots-bound (,this ,class)
-              ,@(loop for connection in connections
-                      for (object func) = (cdadr connection)
-                      collect `(connect! ,object ,func ,this (,name ,@(cdr func))))))))
-
-      `(:slots ,(mapcar #'(lambda (func)
-                            `(,func
-                              (lambda ,clean-args
-                                ,@(when doc (list doc)) ,@clean-decls
-                                (with-slots-bound (,(first clean-args) ,class)
-                                  ,@forms))))
-                        (enumerate-method-descriptors name (mapcar #'cdr (cdr args))))))))
+  ;; Split apart our lambda
+  (form-fiddle:with-destructured-lambda-form (:name name :lambda-list args :docstring doc :declarations decls :forms forms) (cons :slot body)
+    ;; Filter declarations
+    (destructuring-bind (declarations connections methods) (split decls '(connected method) :key #'caadr :test #'string=)
+      ;; Build pure forms
+      (let* ((clean-args (mapcar #'(lambda (a) (if (listp a) (car a) a)) args))
+             (this (first clean-args))
+             (cpp-name (to-method-name name))
+             (cpp-args (mapcar #'cdr (cdr args)))
+             (func-body `(,@(when doc (list doc))
+                          ,@declarations
+                          (with-slots-bound (,(first clean-args) ,class)
+                            ,@forms))))
+        ;; Add our connectors
+        (when connections
+          (add-initializer
+           class *slot-init-priority*
+           `(lambda (,this)
+              (with-slots-bound (,this ,class)
+                ,@(loop for connection in connections
+                        for (object func) = (cdadr connection)
+                        collect `(connect! ,object ,func ,this (,cpp-name ,@(cdr func))))))))
+        ;; Define our methods
+        (when methods
+          (assert (= 1 (length methods)) () "Only one method declaration is allowed.")
+          ;; Exchange name of method if specified
+          (setf name (or (cadadr (first methods)) name))
+          ;; Compile the methods now.
+          (flet ((compile-method (&rest types)
+                   (funcall
+                    (compile NIL `(lambda ()
+                                    (defmethod ,name ((,this ,(class-name class))
+                                                      ,@(loop for type in types
+                                                              for var in (cdr clean-args)
+                                                              collect `(,var ,(ecl-type-for type))))
+                                      ,@func-body))))))
+            (apply #'map NIL #'compile-method cpp-args)))
+        ;; Sputter out the slots
+        (flet ((build-slot (func)
+                 `(,func ,(if methods name `(lambda ,clean-args ,@func-body)))))
+          `(:slots ,(mapcar #'build-slot (enumerate-method-descriptors cpp-name cpp-args))))))))
 
 (define-widget-class-option :overrides (class name args &rest body)
   `(:override
