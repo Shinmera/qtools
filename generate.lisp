59a048b5a17dfcaa1c30f242d139dc6e652021d2
Shinmera <shinmera@tymoon.eu>
2015-02-18 15:19:48 +0100

    Further fixes and advancements, mostly related to making things actually work and reducing the amount of output.

--- generate.lisp
+++ generate.lisp
@@ -6,10 +6,28 @@
 
 (in-package #:org.shirakumo.qtools)
 
-(defparameter *methods* (make-hash-table :test 'equal))
-(defparameter *static-methods* (make-hash-table :test 'equal))
-(defparameter *enums* (make-hash-table :test 'equal))
-(defparameter *constructors* (make-hash-table :test 'equal))
+(defvar *smoke-libs* '(:qt3support :qtcore :qtdbus
+                       :qtdeclarative :qtgui :qthelp
+                       :qtmultimedia :qtnetwork
+                       :qtopengl :qtscript :qtsql
+                       :qtsvg :qttest :qtuitools
+                       :qtwebkit :qtxml :qtxmlpatterns))
+
+(defun load-all-smoke-libs (&optional (libs *smoke-libs*))
+  (dolist (lib libs)
+    (ensure-smoke lib)))
+
+(defvar *methods* (make-hash-table :test 'equal))
+(defvar *static-methods* (make-hash-table :test 'equal))
+(defvar *enums* (make-hash-table :test 'equal))
+(defvar *constructors* (make-hash-table :test 'equal))
+(defvar *target-package* *package*)
+(defvar *unset* (make-symbol "!UNSET!"))
+
+(defun target-symbol (format-string &rest format-args)
+  (let ((name (apply #'format NIL format-string format-args)))
+    (or (find-symbol name *target-package*)
+        (intern name *target-package*))))
 
 (defun write-qclass-name (qclass stream)
   (loop for char across (qclass-name qclass)
@@ -32,12 +50,19 @@
     (write-string "MAKE-" stream)
     (write-qclass-name (qt::qmethod-class method) stream)))
 
-(defun cl-enum-name (method)
+(defun cl-constant-name (method)
   (with-output-to-string (stream)
     (write-char #\+ stream)
     (write-qclass-name (qt::qmethod-class method) stream)
     (write-qmethod-name method stream)
     (write-char #\+ stream)))
+
+(defun cl-variable-name (method)
+  (with-output-to-string (stream)
+    (write-char #\* stream)
+    (write-qclass-name (qt::qmethod-class method) stream)
+    (write-qmethod-name method stream)
+    (write-char #\* stream)))
 
 (defun cl-static-method-name (method)
   (with-output-to-string (stream)
@@ -77,12 +102,17 @@
           (qmethod-name method)
           (mapcar #'qt::qtype-name (qt::list-qmethod-argument-types method))))
 
-(defun generate-docstring (methods)
+(defun generate-method-docstring (methods)
   (format NIL "Call to Qt method ~a
 
-~{See ~/foo::%method-see/~^~%~}"
+~{See ~/org.shirakumo.qtools::%method-see/~^~%~}"
           (clean-method-name (first methods))
           (sort (copy-list methods) #'< :key #'qt::qmethod-argument-number)))
+
+(defun generate-constant-docstring (method)
+  (format NIL "Constant for Qt enum ~a::~a"
+          (qclass-name (qt::qmethod-class method))
+          (clean-method-name method)))
 
 (defmacro with-args ((required optional optional-p) methods &body body)
   (let ((argnums (gensym "ARGNUMS"))
@@ -92,59 +122,63 @@
     `(let* ((,argnums (mapcar #'qt::qmethod-argument-number ,methods))
             (,maxargs (apply #'max ,argnums))
             (,minargs (apply #'min ,argnums))
-            (,required (loop for ,i from 0 to ,minargs
-                             collect (gensym (format NIL "REQ-~d" ,i))))
-            (,optional (loop for ,i from 0 to (- ,maxargs ,minargs)
-                             collect (gensym (format NIL "OPT-~d" ,i))))
+            (,required (loop for ,i from 0 below ,minargs
+                             collect (target-symbol "%REQ-~d" ,i)))
+            (,optional (loop for ,i from 0 below (- ,maxargs ,minargs)
+                             collect (target-symbol "%OPT-~d" ,i)))
             (,optional-p (loop for ,i in ,optional
-                               collect `(,,i NIL ,(make-symbol (format NIL "~a-P" ,i))))))
+                               collect `(,,i NIL ,(target-symbol "~a-P" ,i)))))
        ,@body)))
+
+(defmacro define-extern-inline-fun (name lambda-list &body body)
+  `(progn
+     (export ',name ,(package-name (symbol-package name)))
+     (declaim (inline ,name))
+     (defun ,name ,lambda-list ,@body)))
 
 (defun compile-method (methods)
   (let ((method (clean-method-name (first methods)))
-        (name (intern (cl-method-name (first methods))))
-        (whole (gensym "WHOLE"))
-        (instance (gensym "INSTANCE")))
+        (name (target-symbol (cl-method-name (first methods))))
+        (whole (target-symbol "%WHOLE"))
+        (instance (target-symbol "%INSTANCE")))
     (with-args (reqargs optargs optargs-p) methods
       `(progn
-         (export ',name)
-         (defun ,name (,instance ,@reqargs ,@(when optargs `(&optional ,@optargs-p)))
-           ,(generate-docstring methods)
-           (cond
-             ,@(loop for (arg noop arg-p) in (reverse optargs-p)
-                     for count from 0
-                     collect `(,arg-p
-                               (optimized-call T ,instance ,method ,@reqargs ,@(butlast optargs count))))
-             (T (optimized-call T ,instance ,method ,@reqargs))))
-         (define-compiler-macro ,name (&whole ,whole ,instance ,@reqargs ,@(when optargs `(&optional ,@optargs)))
-           (declare (ignore ,@reqargs ,@optargs))
-           `(optimized-call T ,,instance ,,method ,(cddr ,whole)))))))
-
-(defun map-compile-all-methods (function &optional (table *methods*))
-  (maphash (lambda (name methods)
-             (declare (ignore name))
-             (funcall function (compile-method methods)))
-           table))
+         (define-extern-inline-fun ,name (,instance ,@reqargs ,@(when optargs `(&optional ,@optargs-p)))
+           ,(generate-method-docstring methods)
+           ,(if optargs
+                `(cond
+                   ,@(loop for (arg noop arg-p) in (reverse optargs-p)
+                           for count from 0
+                           collect `(,arg-p
+                                     (optimized-call T ,instance ,method ,@reqargs ,@(butlast optargs count))))
+                   (T (optimized-call T ,instance ,method ,@reqargs)))
+                `(optimized-call T ,instance ,method ,@reqargs)))
+         ,@(when optargs
+             `((define-compiler-macro ,name (&whole ,whole ,instance ,@reqargs ,@(when optargs `(&optional ,@optargs)))
+                 (declare (ignore ,@reqargs ,@optargs))
+                 `(optimized-call T ,,instance ,,method ,(cddr ,whole)))))))))
 
 (defun compile-static-method (class methods)
   (let ((method (clean-method-name (qmethod-name (first methods))))
-        (name (intern (cl-static-method-name (first methods))))
+        (name (target-symbol (cl-static-method-name (first methods))))
         (class-name (qclass-name class))
-        (whole (gensym "WHOLE")))
+        (whole (target-symbol "%WHOLE")))
     (with-args (reqargs optargs optargs-p) methods
       `(progn
-         (export ',name)
-         (defun ,name (,@reqargs ,@(when optargs `(&optional ,@optargs-p)))
-           ,(generate-docstring methods)
-           (cond
-             ,@(loop for (arg noop arg-p) in (reverse optargs-p)
-                     for count from 0
-                     collect `(,arg-p
-                               (optimized-call T ,class-name ,method ,@reqargs ,@(butlast optargs count))))
-             (T (optimized-call T ,class-name ,method ,@reqargs))))
-         (define-compiler-macro ,name (&whole ,whole ,@reqargs ,@(when optargs `(&optional ,@optargs)))
-           (declare (ignore ,@reqargs ,@optargs))
-           `(optimized-call T ,,class-name ,,method ,(cdr ,whole)))))))
+         (define-extern-inline-fun ,name (,@reqargs ,@(when optargs `(&optional ,@optargs-p)))
+           ,(generate-method-docstring methods)
+           ,(if optargs
+                `(cond
+                   ,@(loop for (arg noop arg-p) in (reverse optargs-p)
+                           for count from 0
+                           collect `(,arg-p
+                                     (optimized-call T ,class-name ,method ,@reqargs ,@(butlast optargs count))))
+                   (T (optimized-call T ,class-name ,method ,@reqargs)))
+                `(optimized-call T ,class-name ,method ,@reqargs)))
+         ,@(when optargs
+             `((define-compiler-macro ,name (&whole ,whole ,@reqargs ,@(when optargs `(&optional ,@optargs)))
+                 (declare (ignore ,@reqargs ,@optargs))
+                 `(optimized-call T ,,class-name ,,method ,(cdr ,whole)))))))))
 
 (defun map-compile-static-methods (function methods)
   (let ((bundle (make-hash-table :test 'eq)))
@@ -154,49 +188,70 @@
           for methods being the hash-values of bundle
           do (funcall function (compile-static-method class methods)))))
 
+(defun compile-constructor (methods)
+  (let ((name (target-symbol (cl-constructor-name (first methods))))
+        (class (qclass-name (qt::qmethod-class (first methods))))
+        (whole (target-symbol "%WHOLE")))
+    (with-args (reqargs optargs optargs-p) methods
+      `(progn
+         (define-extern-inline-fun ,name (,@reqargs ,@(when optargs `(&optional ,@optargs-p)))
+           ,(generate-method-docstring methods)
+           ,(if optargs
+                `(cond
+                   ,@(loop for (arg noop arg-p) in (reverse optargs-p)
+                           for count from 0
+                           collect `(,arg-p
+                                     (optimized-new ,class ,@reqargs ,@(butlast optargs count))))
+                   (T (optimized-new ,class ,@reqargs)))
+                `(optimized-new ,class ,@reqargs)))
+         ,@(when optargs
+             `((define-compiler-macro ,name (&whole ,whole ,@reqargs ,@(when optargs `(&optional ,@optargs)))
+                 (declare (ignore ,@reqargs ,@optargs))
+                 `(optimized-new ,,class ,(cdr ,whole)))))))))
+
+(defmacro define-qt-constant (name (class method) &optional documentation)
+  (let ((memovar (target-symbol "*MEMO-~a*" name)))
+    `(progn
+       (export ',name ,(package-name (symbol-package name)))
+       (defvar ,memovar)
+       (defun ,memovar ()
+         (cond ((boundp ',memovar) ,memovar)
+               ((find-qclass ,class)
+                (setf ,memovar (optimized-call T ,class ,method)))
+               (T (error ,(format NIL "Cannot fetch enum value ~a::~a. Does the class exist?"
+                                  class method)))))
+       (define-symbol-macro ,name (,memovar))
+       ,@(when documentation
+           `((setf (documentation ',name 'variable) ,documentation))))))
+
+(defun map-compile-constants (function methods)
+  (loop for method in methods
+        for class = (qclass-name (qt::qmethod-class method))
+        for constant = (target-symbol (cl-constant-name method))
+        do (funcall
+            function
+            `(define-qt-constant ,constant
+                 (,(qclass-name (qt::qmethod-class method))
+                  ,(qmethod-name method))
+               ,(generate-constant-docstring method)))))
+
+(defun map-compile-all-methods (function &optional (table *methods*))
+  (maphash (lambda (name methods)
+             (declare (ignore name))
+             (funcall function (compile-method methods)))
+           table))
+
 (defun map-compile-all-static-methods (function &optional (table *static-methods*))
   (maphash (lambda (name methods)
              (declare (ignore name))
              (map-compile-static-methods function methods))
            table))
 
-(defun compile-constructor (methods)
-  (let ((name (intern (cl-constructor-name (first methods))))
-        (class (qclass-name (qt::qmethod-class (first methods))))
-        (whole (gensym "WHOLE")))
-    (with-args (reqargs optargs optargs-p) methods
-      `(progn
-         (export ',name)
-         (defun ,name (,@reqargs ,@(when optargs `(&optional ,@optargs-p)))
-           ,(generate-docstring methods)
-           (cond
-             ,@(loop for (arg noop arg-p) in (reverse optargs-p)
-                     for count from 0
-                     collect `(,arg-p
-                               (optimized-new ,class ,@reqargs ,@(butlast optargs count))))
-             (T (optimized-new ,class ,@reqargs))))
-         (define-compiler-macro ,name (&whole ,whole ,@reqargs ,@(when optargs `(&optional ,@optargs)))
-           (declare (ignore ,@reqargs ,@optargs))
-           `(optimized-new ,,class ,(cdr ,whole)))))))
-
 (defun map-compile-all-constructors (function &optional (table *constructors*))
   (maphash (lambda (name methods)
              (declare (ignore name))
              (funcall function (compile-constructor methods)))
            table))
-
-(defun map-compile-constants (function methods)
-  (loop for method in methods
-        for class = (qclass-name (qt::qmethod-class method))
-        for name = (intern (cl-enum-name method))
-        do (funcall
-            function
-            `(progn
-               (export ',name)
-               (defconstant ,name
-                 (load-time-value
-                  (when (find-class ,class)
-                    (optimized-call T ,class ,(qmethod-name method)))))))))
 
 (defun map-compile-all-constants (function &optional (table *enums*))
   (maphash (lambda (name methods)
@@ -237,9 +292,12 @@
     (funcall mapper
              (lambda (form)
                (incf i)
-               (when (= 0 (mod i 100))
+               (when (= 0 (mod i 1000))
                  (format T "~&; On ~dth form..." i))
-               (print form stream)
+               (dolist (form (if (eql (car form) 'progn)
+                                 (cdr form)
+                                 (list form)))
+                 (print form stream))
                (format stream "~%")))
     (format T "~&; ~d forms processed." i)))
 
@@ -260,12 +318,17 @@
   (let* ((package (cond ((typep package 'package))
                         ((find-package package) (find-package package))
                         (T (make-package package))))
-         (*package* package))
+         (*target-package* package)
+         (*package* (find-package '#:cl-user)))
     (with-open-file (stream pathname :direction :output :if-exists if-exists)
       (format stream ";;;;; Automatically generated file to map Qt methods and enums to CL functions and constants.~%")
       (format stream ";;;;; See QTOOLS:WRITE-EVERYTHING-TO-FILE~%")
-      (print `(defpackage ,(package-name package)) stream)
-      (print `(in-package ,(package-name package)) stream)
+      (print `(in-package #:cl-user) stream)
+      (print `(eval-when (:compile-toplevel :load-toplevel :execute)
+                (unless (find-package ,(package-name package))
+                  (make-package ,(package-name package)))) stream)
       (funcall body-processor stream))))
 
-;; FIXME: For constants, all smoke contribs need to be available at load-time...
+;; FIXME: Duplicate definitions
+;; FIXME: Standard operators (* / + -)
+;; FIXME: Setters
