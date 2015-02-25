7b171fff15cc2198b3063762175639c7b41fb3c4
Shinmera <shinmera@tymoon.eu>
2015-02-25 14:34:08 +0100

    OpenGL example

---
+++ examples/opengl/opengl.lisp
@@ -0,0 +1,65 @@
+#|
+ This file is a part of Qtools
+ (c) 2015 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(defpackage #:qtools-opengl
+  (:nicknames #:org.shirakumo.qtools.opengl)
+  (:use #:cl+qt)
+  (:export #:main))
+(in-package #:qtools-opengl)
+(named-readtables:in-readtable :qtools)
+
+(define-widget opengl (QGLWidget)
+  ((angle :initform 0)
+   (angle-delta :initform 1)))
+
+(define-subwidget (opengl timer) (q+:make-qtimer opengl)
+  (setf (q+:single-shot timer) NIL)
+  (q+:start timer (round 1000/30)))
+
+(define-slot (opengl update) ()
+  (declare (connected timer (timeout)))
+  (incf angle angle-delta)
+  (q+:repaint opengl))
+
+(define-initializer (opengl setup)
+  (setf (q+:window-title opengl) "Qtools OpenGL Example")
+  (setf (q+:fixed-size opengl) (values 500 500)))
+
+(define-override (opengl key-release-event) (ev)
+  (cond ((= (q+:key ev) (q+:qt.key-left))
+         (decf angle-delta))
+        ((= (q+:key ev) (q+:qt.key-right))
+         (incf angle-delta)))
+  (stop-overriding))
+
+(define-override (opengl paint-event) (ev)
+  (declare (ignore ev))
+  (with-finalizing ((painter (q+:make-qpainter opengl)))
+    (q+:begin-native-painting painter)
+
+    (gl:push-matrix)
+    (gl:translate 250 250 0)
+    (gl:rotate angle 0 0 1)
+    (gl:with-primitives :quads
+      (gl:color 1 0 0)
+      (gl:vertex -50 -50)
+      (gl:color 0 1 0)
+      (gl:vertex 50 -50)
+      (gl:color 0 0 1)
+      (gl:vertex 50 50)
+      (gl:color 1 1 1)
+      (gl:vertex -50 50))
+    (gl:pop-matrix)
+
+    (q+:end-native-painting painter)
+    (let ((font (q+:make-qfont "Monospace" 10)))
+      (setf (q+:style-hint font) (q+:qfont.type-writer))
+      (setf (q+:font painter) font))
+    (q+:draw-text painter 5 15 "<left>:  Decrease speed")
+    (q+:draw-text painter 5 30 "<right>: Increase speed")))
+
+(defun main ()
+  (with-main-window (window (make-instance 'opengl))))
---
+++ examples/opengl/qtools-opengl.asd
@@ -0,0 +1,19 @@
+#|
+ This file is a part of Qtools
+ (c) 2015 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:cl-user)
+(asdf:defsystem qtools-opengl
+  :name "Qtools-Opengl"
+  :version "1.0.0"
+  :license "Artistic"
+  :author "Nicolas Hafner <shinmera@tymoon.eu>"
+  :maintainer "Nicolas Hafner <shinmera@tymoon.eu>"
+  :description "An example combinging Qt and OpenGL."
+  :homepage "https://github.com/Shinmera/qtools"
+  :serial T
+  :components ((:file "opengl"))
+  :depends-on (:qtools :qtcore :qtgui :qtopengl
+               :cl-opengl))
